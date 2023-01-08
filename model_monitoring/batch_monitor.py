69f43ab0308dc56368583e4c13b92fa43908720d
Chinedu Ezeofor <Chineiduezeofor@gmail.com>
2023-01-08 02:18:18 +0100

    modified:   model_monitoring/batch_monitor.py 	deleted:    model_monitoring/batch_monitoring.py 	new file:   model_monitoring/feat_engineering.py

--- model_monitoring/batch_monitor.py
+++ model_monitoring/batch_monitor.py
@@ -1,11 +1,14 @@
 import os
 import json
 import typing as tp
+from datetime import timedelta
 import joblib
 
+import numpy as np
 import pandas as pd
 from pymongo import MongoClient
 from prefect import task, flow, get_run_logger
+from prefect.tasks import task_input_hash
 from evidently import ColumnMapping
 from evidently.report import Report
 from evidently.metric_preset import (
@@ -15,7 +18,6 @@ from evidently.metric_preset import (
 )
 
 MONGODB_ADDRESS = os.getenv("MONGODB_ADDRESS", "mongodb://127.0.0.1:27018")
-logger = get_run_logger()
 
 
 @task(retries=3, retry_delay_seconds=10)
@@ -36,15 +38,15 @@ def upload_target_to_db(*, filename: str) -> None:
             for line in file_in.readlines():
                 row = line.split(",")
                 # Update the data in the collection
-                filter = row[0]
-                updated_val = {"$set": {"target": round(row[1])}}
+                filter = {"id": row[0]}
+                updated_val = {"$set": {"target": round(float(row[1]))}}
                 collection.update_one(filter=filter, update=updated_val)
 
 
 def predict(*, data: pd.DataFrame) -> float:
     """This is used to make predictions on unseen data using
     the trained model."""
-    MODEL_FILE = os.getenv("MODEL_FILE", "model.pkl")
+    MODEL_FILE = os.getenv("MODEL_FILE", "./prediction_service/model.pkl")
     with open(MODEL_FILE, "rb") as file:
         model = joblib.load(file)
 
@@ -53,7 +55,12 @@ def predict(*, data: pd.DataFrame) -> float:
     return pred
 
 
-@task(retries=3, retry_delay_seconds=10)
+@task(
+    retries=3,
+    retry_delay_seconds=10,
+    cache_key_fn=task_input_hash,
+    cache_expiration=timedelta(days=1),
+)
 def load_ref_data(*, filename: str) -> pd.DataFrame:
     """This is used to load the reference data.
 
@@ -66,6 +73,7 @@ def load_ref_data(*, filename: str) -> pd.DataFrame:
     ref_data (Pandas DF): DF containing the loaded training data.
     """
     ref_data = pd.read_parquet(filename)
+    logger = get_run_logger()
 
     def calculate_trip_duration(*, data: pd.DataFrame) -> np.ndarray:
         """This returns a DF containing the calculated trip_duration in minutes."""
@@ -79,14 +87,14 @@ def load_ref_data(*, filename: str) -> pd.DataFrame:
         trip_duration = round(trip_duration.dt.total_seconds() / MINS)
         return trip_duration
 
-    logger.info("Calculating target and makin predictions ...")
+    logger.info("Calculating target and making predictions ...")
     ref_data["target"] = calculate_trip_duration(data=ref_data)
     ref_data["predictions"] = predict(data=ref_data)
 
     return ref_data
 
 
-@flow(retries=3, retry_delay_seconds=10)
+@task(retries=3, retry_delay_seconds=10)
 def run_evidently(ref_data: pd.DataFrame, curr_data: pd.DataFrame) -> tp.Tuple:
     """This is used to batch monitor the model. It calculates Data drift,
     data quality, and regression report using Evidently.
@@ -103,6 +111,8 @@ def run_evidently(ref_data: pd.DataFrame, curr_data: pd.DataFrame) -> tp.Tuple:
     # Ensure that size of reference data == current data
     data_size = curr_data.shape[0]
     ref_data = ref_data.sample(n=data_size, random_state=123)
+
+    logger = get_run_logger()
 
     column_mapping = ColumnMapping()
     numerical_features = ["trip_distance", "total_amount"]
@@ -153,6 +163,7 @@ def fetch_data():
 @task(retries=3, retry_delay_seconds=10)
 def save_report_logs(*, json_report: tp.Dict):
     """This is used to save the metrics in MongoDB."""
+    logger = get_run_logger()
     db_name = "prediction_service"
     collection_name = "report"
     with MongoClient(MONGODB_ADDRESS) as client:
@@ -165,6 +176,7 @@ def save_report_logs(*, json_report: tp.Dict):
 @task(retries=3, retry_delay_seconds=10)
 def save_html_report(*, report: tp.Any, name: str):
     """This is used to save the metrics report as HTML."""
+    logger = get_run_logger()
     report.save(f"{name}.html")
     logger.info("Report saved as HTML!")
 
@@ -172,27 +184,30 @@ def save_html_report(*, report: tp.Any, name: str):
 @flow
 def run_batch_analyses():
     """This is the workflow for running the batch model monitoring analyses."""
-    upload_target_to_db("target.csv")
-    print("Fetching reference data ...")
+    logger = get_run_logger()
 
+    logger.info("Updating records in MongoDB ...")
+    upload_target_to_db(filename="target.csv")
+
+    logger.info("Fetching reference data ...")
     ref_data = load_ref_data(
-        "./evidently_service/datasets/yellow_tripdata_2022-01.parquet"
+        filename="./evidently_service/datasets/yellow_tripdata_2022-01.parquet"
     )
-    print("Fetching data from MongoDB ...")
+    logger.info("Fetching data from MongoDB ...")
     data = fetch_data()
 
-    print("Running Evidently Service ...")
+    logger.info("Running Evidently Service ...")
     data_drift_n_qty_report, regression_report, json_report = run_evidently(
-        ref_data, data
+        ref_data=ref_data, curr_data=data
     )
 
-    print("Saving reports to MongoDB ...")
+    logger.info("Saving reports to MongoDB ...")
     save_report_logs(json_report=json_report)
 
-    print("Saving reports as HTML ...")
-    save_html_report(data_drift_n_qty_report)
-    save_html_report(regression_report)
+    logger.info("Saving reports as HTML ...")
+    save_html_report(report=data_drift_n_qty_report)
+    save_html_report(report=regression_report)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run_batch_analyses()
