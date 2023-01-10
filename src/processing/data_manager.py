e43af8f5078aba748e91bde752081c597f3df43c
Chinedu Ezeofor <Chineiduezeofor@gmail.com>
2023-01-10 15:19:23 +0100

    modified:   logging.config 	modified:   src/processing/data_manager.py

--- src/processing/data_manager.py
+++ src/processing/data_manager.py
@@ -24,15 +24,38 @@ from src.config.core import DATA_FILEPATH, SRC_ROOT, TRAINED_MODELS_FILEPATH, co
 from src.config.schema import ValidateInputSchema, ValidateTrainingData
 
 
-def _set_up_logger(delim: str = "::") -> tp.Any:
-    """This is used to create a basic logger."""
-    format_ = f"[%(levelname)s] {delim} %(asctime)s {delim} %(message)s"
-    logging.basicConfig(level=logging.INFO, format=format_)
-    logger = logging.getLogger(__name__)
+def custom_logger():
+    """This is used to create a custom logger that saves the output
+    to the path: logs/
+
+    Sample messages:
+    ----------------
+        logger.debug("Used for debugging your code.")
+        logger.info("Informative messages from your code.")
+        logger.warning("Everything works but there is something to be aware of.")
+        logger.error("There's been a mistake with the process.")
+        logger.critical("There is something terribly wrong and process may terminate.")
+
+    """
+    import logging.config
+    from rich.logging import RichHandler
+    from pathlib import Path
+
+    BASE_DIR = Path(__name__).absolute().parent
+    LOGS_DIR = Path(BASE_DIR, "logs")
+    # Create directory
+    LOGS_DIR.mkdir(parents=True, exist_ok=True)
+
+    # Use config file to initialize logger
+    CONFIG_DIR = BASE_DIR
+    logging.config.fileConfig(Path(CONFIG_DIR, "logging.config"))
+    logger = logging.getLogger()
+    logger.handlers[0] = RichHandler(markup=True)  # set rich handler
     return logger
 
 
-logger = _set_up_logger()
+# logger = _set_up_logger()
+logger = custom_logger()
 Estimator = tp.Union[Pipeline, tp.Any]  # Alias for estimator
 
 
@@ -75,7 +98,7 @@ def load_data(*, filename: tp.Union[str, Path], uri: bool = False) -> pd.DataFra
 
     if filename.endswith("parquet"):
 
-        def calculate_trip_duration(data: pd.DataFrame) ->  np.ndarray:
+        def calculate_trip_duration(data: pd.DataFrame) -> np.ndarray:
             """This returns a DF containing the calculated trip_duration in minutes."""
             data = data.copy()
             # Convert to minutes
