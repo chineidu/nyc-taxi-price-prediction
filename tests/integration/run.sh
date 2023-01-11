8ca45ab8683537af477dee2f4d2a7e9763b7e6d3
Chinedu Ezeofor <Chineiduezeofor@gmail.com>
2023-01-11 21:45:47 +0100

    new file:   tests/integration/.dockerignore 	modified:   tests/integration/run.sh 	modified:   tests/integration/test_docker_integration.py

--- tests/integration/run.sh
+++ tests/integration/run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Terminate the script if an error occurs
+set -e 
+
 # Change to the dir where the script is located
 cd "$(dirname "$0")"
 
@@ -7,28 +10,29 @@ DOCKER_FILE="Dockerfile"
 
 
 if [ ${IMAGE_NAME} == ""]; then
-    TAG=`date +"%Y-%m-%d_%H-%M"`
+    TAG=`date +"%Y-%m-%d_%H"`
     export IMAGE_NAME="stream-model-duration:${TAG}"
     echo "IMAGE_NAME is not set ... building a new image with tag: ${IMAGE_NAME}"
-    docker build -t ${IMAGE_NAME} .
+    docker build -t ${IMAGE_NAME} -f ${DOCKER_FILE} .
 else
     echo "No need to to build image $IMAGE_NAME"
 fi
 
+export IMAGE_NAME="stream-model-duration:${TAG}"
 
 docker-compose up -d
 sleep 3
 
 ERROR_CODE="$?"
 
-python test_docker.py
+python test_docker_integration.py
+
 if [ ${ERROR_CODE} != 0 ]; then
     # Run
     docker-compose logs
-    docker-compose down
-    exit ${ERROR_CODE}
-
 fi
 
+docker-compose down
+exit ${ERROR_CODE}
 
-echo "Done!"
+# echo "Done!"
--- tests/integration/test_docker_integration.py
+++ tests/integration/test_docker_integration.py
@@ -10,43 +10,45 @@ from pprint import pprint as pp
 from deepdiff import DeepDiff
 
 
-def test_docker_integration() -> None:
-    # Given
-    expected_output = {
-        "predictions": [
-            {
-                "model": "ride_duration_prediction_model",
-                "version": "123",
-                "prediction": {"ride_duration": 9.47, "ride_id": 123},
-            }
-        ]
-    }
 
-    # When
-    event = {
-        "Records": [
-            {
-                "kinesis": {
-                    "kinesisSchemaVersion": "1.0",
-                    "partitionKey": "1",
-                    "sequenceNumber": "49636674106407327729976759147118224854495581328161898498",
-                    "data": "eyJyaWRlIjogeyJET0xvY2F0aW9uSUQiOiAxMzAsICJwYXltZW50X3R5cGUiOiAyLCAiUFVMb2NhdGlvbklEIjogMTE1LCAiUmF0ZWNvZGVJRCI6IDEuMCwgInRvdGFsX2Ftb3VudCI6IDEyLjIsICJ0cGVwX3BpY2t1cF9kYXRldGltZSI6ICIyMDIyLTAyLTAxIDE0OjI4OjA1IiwgInRyaXBfZGlzdGFuY2UiOiAyLjM0LCAiVmVuZG9ySUQiOiAxfSwgInJpZGVfaWQiOiAxMjN9",
-                    "approximateArrivalTimestamp": 1672650485.394,
-                },
-                "eventSource": "aws:kinesis",
-                "eventVersion": "1.0",
-                "eventID": "shardId-000000000000:49636674106407327729976759147118224854495581328161898498",
-                "eventName": "aws:kinesis:record",
-                "invokeIdentityArn": "arn:aws:iam::126946216053:role/lambda_kinesis_custom",
-                "awsRegion": "eu-west-1",
-                "eventSourceARN": "arn:aws:kinesis:eu-west-1:126946216053:stream/ride_events",
-            }
-        ]
-    }
-    url = "http://localhost:8080/2015-03-31/functions/function/invocations"
-    respose = requests.post(url, json=event).json()
-    pp(respose)
-    d_diff = DeepDiff(expected_output, respose)  # It compares the inputs
+# Given
+expected_output = {
+    "predictions": [
+        {
+            "model": "ride_duration_prediction_model",
+            "version": "123",
+            "prediction": {"ride_duration": 9.47, "ride_id": 123},
+        }
+    ]
+}
 
-    # Then
-    assert "values_changed" not in d_diff
+# When
+event = {
+    "Records": [
+        {
+            "kinesis": {
+                "kinesisSchemaVersion": "1.0",
+                "partitionKey": "1",
+                "sequenceNumber": "49636674106407327729976759147118224854495581328161898498",
+                "data": "eyJyaWRlIjogeyJET0xvY2F0aW9uSUQiOiAxMzAsICJwYXltZW50X3R5cGUiOiAyLCAiUFVMb2NhdGlvbklEIjogMTE1LCAiUmF0ZWNvZGVJRCI6IDEuMCwgInRvdGFsX2Ftb3VudCI6IDEyLjIsICJ0cGVwX3BpY2t1cF9kYXRldGltZSI6ICIyMDIyLTAyLTAxIDE0OjI4OjA1IiwgInRyaXBfZGlzdGFuY2UiOiAyLjM0LCAiVmVuZG9ySUQiOiAxfSwgInJpZGVfaWQiOiAxMjN9",
+                "approximateArrivalTimestamp": 1672650485.394,
+            },
+            "eventSource": "aws:kinesis",
+            "eventVersion": "1.0",
+            "eventID": "shardId-000000000000:49636674106407327729976759147118224854495581328161898498",
+            "eventName": "aws:kinesis:record",
+            "invokeIdentityArn": "arn:aws:iam::126946216053:role/lambda_kinesis_custom",
+            "awsRegion": "eu-west-1",
+            "eventSourceARN": "arn:aws:kinesis:eu-west-1:126946216053:stream/ride_events",
+        }
+    ]
+}
+url = "http://localhost:8080/2015-03-31/functions/function/invocations"
+respose = requests.post(url, json=event).json()
+pp(f"response: {respose}")
+print()
+d_diff = DeepDiff(expected_output, respose)  # It compares the inputs
+print(f"d_diff: {d_diff}")
+
+# Then
+assert "values_changed" not in d_diff
